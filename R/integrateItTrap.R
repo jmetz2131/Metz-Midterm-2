72eccbe6b345cee8b592bbfd58891fcde6633eba
jmetz2131 <jacob.metz@wustl.edu>
2016-04-12 10:15:25 -0500

    Added comments to integrateIt function
    Still unsure about how I’ve set my x vector within the function itself.
    I added comments explaining why I coded this function the way that I
    did. I feel like something might be off with the x values that are fed
    to the function itself, something I will make sure to check before
    replicating this code to make it ready for Simpson’s Rule.

--- R/integrateItTrap.R
+++ R/integrateItTrap.R
@@ -1,14 +1,32 @@
 ##Setting up the function
 integrateIt <- function(x, y, a, b, n, Rule){ 
-  x <- seq(a, b, by=n)
-  penultimate <- length(y)-1
-  h <- (b-a)/n
+  ##The arguments in my integrateIt function are as follows: 
+    ## x = a vector of x values, y = a vector of f(x) values, a = the starting point 
+    ##from which the user chooses to integrate, b = the ending point to which 
+    ##the user chooses to stop the integration, n = the number of partitions the 
+    ##user wants, Rule = whether the user is using the Trapezoidal Rule or Simpson's Rule
+  x <- seq(a, b, by=n) ##UNSURE OF THIS
+  
+  ##In the actual equation itself for the trapezoidal rule, the y value of every
+  ##term is multiplied by 2 except for the first and last x values put into the equation.
+  ##The first and last x values are represented in my equation by y[1] and tail(y, n=1), respectively.
+  ##The following value penultimate represents the second to last value. I needed it to
+  ##sum the y values of the second term to the penultimate term as dictated by the number
+  ##of partitions the user chooses.
+  penultimate <- length(y)-1 
+  
+  h <- (b-a)/n ##This represents the delta x value and takes the values set in the arguments
+  ##to calculate h. 
+  
   if(Rule=="Trap"){
-    t_answer <- (h/2)*(y[1]+tail(y, n=1)+2*(sum(y[2:penultimate])))
+    t_answer <- (h/2)*(y[1]+tail(y,n=1)+2*(sum(y[2:penultimate])))
+    ##t_answer is the actual Trapezoidal rule itself: 
+    ##h/2*(f(x0)+2(f(x1)+...f(xn-1))+f(xn))
   }
-  return(t_answer)
+  return(t_answer) ##This outputs the calculation from t_answer itself
 }
 
+##These are my test vectors for integrating from 1 to 5 on the equation 2x.
 x<-seq(1, 5, by=1)
 y<-c(2,4,6,8,10)
 
