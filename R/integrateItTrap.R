e8e6be9e3c056cd4f6b17f127d865b16ee512df0
jmetz2131 <jacob.metz@wustl.edu>
2016-04-13 13:50:50 -0500

    Setting up both integrateIt functions
    I am still figuring out how to properly make these functions work. I
    believe I am close and I am confident that I will be able to finish
    this midterm this time around. It’s taken me a while to figure out
    these functions and I am going to clean up the Simpson’s Rule one but I
    believe I am closer to the correct answer.

--- R/integrateItTrap.R
+++ R/integrateItTrap.R
@@ -1,11 +1,13 @@
 ##Setting up the function
-integrateIt <- function(x, y, a, b, n, Rule){ 
+integrateIt <- function(x, y, a, b, Rule){ 
   ##The arguments in my integrateIt function are as follows: 
     ## x = a vector of x values, y = a vector of f(x) values, a = the starting point 
     ##from which the user chooses to integrate, b = the ending point to which 
-    ##the user chooses to stop the integration, n = the number of partitions the 
-    ##user wants, Rule = whether the user is using the Trapezoidal Rule or Simpson's Rule
-  x <- seq(a, b, by=n) ##UNSURE OF THIS
+    ##the user chooses to stop the integration, Rule = whether the user is 
+    ##using the Trapezoidal Rule or Simpson's Rule
+  
+  ##For clarification, I am using f(x) values in this function, so am dealing with
+  ##y in this function.
   
   ##In the actual equation itself for the trapezoidal rule, the y value of every
   ##term is multiplied by 2 except for the first and last x values put into the equation.
@@ -14,6 +16,10 @@ integrateIt <- function(x, y, a, b, n, Rule){
   ##sum the y values of the second term to the penultimate term as dictated by the number
   ##of partitions the user chooses.
   penultimate <- length(y)-1 
+  
+  n <- length(y) ##n represents the number of partitions the user uses, which in this case
+  ##is simply the number of y values themselves. I chose to use length(y) because the y values
+  ##are going to be used in the actual Trapezoidal rule equation itself
   
   h <- (b-a)/n ##This represents the delta x value and takes the values set in the arguments
   ##to calculate h. 
@@ -27,7 +33,9 @@ integrateIt <- function(x, y, a, b, n, Rule){
 }
 
 ##These are my test vectors for integrating from 1 to 5 on the equation 2x.
-x<-seq(1, 5, by=1)
-y<-c(2,4,6,8,10)
+test_x<-seq(1, 6, by=1)
+test_y<-seq(2, 12, by=2)
 
-integrateIt(x, y, 1, 5, 4,"Trap")
+length(test_y)
+
+integrateIt(test_x, test_y, 1, 6,"Trap")
