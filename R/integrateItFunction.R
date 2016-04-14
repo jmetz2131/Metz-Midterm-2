1d074a9b5ce674c336624c397a1b337e3ffcb3b1
jmetz2131 <jacob.metz@wustl.edu>
2016-04-13 20:35:17 -0500

    Adding Comments
    I still need to appropriately put the files in the right place to
    prepare for the package development, but this is the current and
    correct functions for the Trapezoidal Rule and Simpson’s Rule.

--- R/integrateItFunction.R
+++ R/integrateItFunction.R
@@ -9,17 +9,18 @@ integrateIt <- function(x, y, a, b, Rule){
   ##For clarification, I am using f(x) values in this function, so am dealing with
   ##y in this function.
   
-  n <- length(y)-1 ##n represents the number of partitions the user uses, which in this case
-  ##is simply the number of y values themselves. I chose to use length(y) because the y values
-  ##are going to be used in the actual equations themselves.
-  
+  n <- length(y)-1 ##n represents the number of subdivisions the user uses. 
+  ##I subtracted one from length(y) to ensure that the width of each subdivision
+  ##was 1 like Professor Montgomery and I discussed. For example, an integration from
+  ##1 to 9 would have 8 subdivisions and this code reflects that.
+ 
   h <- (b-a)/n ##This represents the delta x value and takes the values set in the arguments
   ##to calculate h. 
   
   if(Rule=="Trap"){
     penultimate <- length(y)-1   ##In the actual equation itself for the trapezoidal rule, the y value of every
     ##term is multiplied by 2 except for the first and last x values put into the equation.
-    ##The first and last x values are represented in my equation by y[1] and tail(y, n=1), respectively.
+    ##The first and last x values are represented in my equation by y[1] and y[length(y)], respectively.
     ##The following value penultimate represents the second to last value. I needed it to
     ##sum the y values of the second term to the penultimate term as dictated by the number
     ##of partitions the user chooses.
@@ -30,11 +31,20 @@ integrateIt <- function(x, y, a, b, Rule){
   }
   
   if(Rule=="Simp"){
-    noEndPoints <- y[-c(1, length(y))]
+    noEndPoints <- y[-c(1, length(y))] 
+    ##Given the Simpson's Rule, I used this code to remove the first and last
+    ##values because those two values will not be multiplied in any way in the
+    ##rule itself.
     
     even <- noEndPoints[c(TRUE, FALSE)]
+    ##Here I used noEndPoints and used the TRUE, FALSE pattern above  
+    ##to isolate the even numbered indices so I know which f(x) values to
+    ##multiply by 4.
     
     odd <- noEndPoints[c(FALSE, TRUE)]
+    ##Here I used noEndPoints and used the TRUE, FALSE pattern above 
+    ##to isolate the odd numbered indices so I know which f(x) values to 
+    ##multiply by 2.
     
     answer <- (h/3)*(y[1]+y[length(y)]+4*(sum(even)) + 2*(sum(odd)))
     ##answer is the actual Simpson's rule itself: 
@@ -43,13 +53,15 @@ integrateIt <- function(x, y, a, b, Rule){
   return(answer) ##This outputs the calculation from the Rule the user specifies
 }
 
-##These are my test vectors for integrating from 1 to 5 on the equation 2x.
-test_x<-seq(1, 7, by=1)
-test_y<-c(sqrt(1:7))
+##These are my test vectors for integrating from 1 to 9 on the equation sqrt(x).
+test_x<-seq(1, 9, by=1) ##x values from 1 to 9 for y=sqrt(x)
+test_y<-c(sqrt(1:9)) ##y values for f(1) to f(9) for y=sqrt(x)
 
-length(test_y)
+integrateIt(test_x, test_y, 1, 9, "Trap") ##This correctly comes out to 17.306
 
-integrateIt(test_x, test_y, 1, 7,"Trap")
+integrateIt(test_x,test_y, 1, 9, "Simp") ##This correctly comes out to 17.33209
 
-integrateIt(test_x,test_y, 1,7,"Simp")
+##The definite integral value of y=sqrt(x) from 1 to 9 is 17.333. The Simpson's Rule
+##equation is supposed to be more accurate than the Trapezoidal Rule equation and they 
+##both come close to the actual definite integral value.
 
